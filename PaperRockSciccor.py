9909f5742ac5f123e09a57f6d3a6bab78335778d
Asif Azamuddin <105183445+SipSipi@users.noreply.github.com>
2022-05-17 12:03:07 +0800

    Update PaperRockSciccor.py

--- PaperRockSciccor.py
+++ PaperRockSciccor.py
@@ -7,8 +7,19 @@ PRS = [
     message= "Paper, Rock, Sciccor?",
     choices= ['Paper', 'Rock', 'Sciccor'])
 ]
+
 a = ['Paper','Rock','Sciccor']
 
+c = inquirer.prompt(PRS).
+#print (c)
 
-if PRS == random.choice(a):
+if c == random.choice(a):
     print (f"no winner, both player chose {a}")
+elif c != random.choice(a)[1] :
+    print (str(c) + 'win')
+elif c != random.choice(a)[2] :
+    print (str(c) + 'win')
+elif c != random.choice(a)[0] :
+    print (str(c) + 'win') 
+else :
+    print (random.choice(a) + ' win')  
